fb3461d480ad7707a098fe30f2fa3f0890e097e0
oseniy <ars7080947@gmail.com>
2025-04-08 10:23:51 +0300

    blue bg and load fn

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, StatusBar} from "react-native";
+import * as Location from 'expo-location';
+import Loading from "./loading";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>vsem privet</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#87CEFA" barStyle="dark-content" />
+      <Loading />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#87CEFA"
+  }
+})
