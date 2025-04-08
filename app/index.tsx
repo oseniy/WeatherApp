2f6602dc7bd1823e0d779132e124e0ce9d60e6c5
oseniy <ars7080947@gmail.com>
2025-04-08 10:35:32 +0300

    getLocation made

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,27 @@
 import { Text, View, StyleSheet, StatusBar} from "react-native";
+import React from 'react';
 import * as Location from 'expo-location';
 import Loading from "./loading";
 
-export default function Index() {
-  return (
+export default class extends React.Component {
+
+  GetLocation = async () => {
+    const location = await Location.getCurrentPositionAsync()
+    console.log(location)
+  }
+  componentDidMount(): void {
+    this.GetLocation()
+  }
+
+  render () {
+    return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#87CEFA" barStyle="dark-content" />
       <Loading />
     </View>
-  );
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
