5a35f1021ac666b86680967311f2a2fcf8d0b1ec
oseniy <ars7080947@gmail.com>
2025-04-10 21:51:59 +0300

    switched to js

---
+++ app/index.js
@@ -0,0 +1,55 @@
+import { Text, View, StyleSheet, StatusBar, Alert} from "react-native";
+import React from 'react';
+import * as Location from 'expo-location';
+import axios from 'axios'
+import Loading from "./loading";
+import Weather from "./weather";
+
+const API_KEY = '32da35c7fdfa601889d7046e111d7cd3'
+
+export default class extends React.Component {
+
+  state = {
+    isLoading: true
+  }
+
+  GetWeather = async (latitude, longitude) => {
+    const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}
+    &units=metric`)
+    this.setState({isLoading: false, temp: data.main.temp})
+  }  
+
+  GetData = async () => {
+    try {
+      await Location.requestForegroundPermissionsAsync()
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync()
+      this.GetWeather(latitude, longitude)
+    } catch (error) {
+      Alert.alert('Не могу определить местоположение', ":(")
+    }
+    
+  }
+  componentDidMount() {
+    this.GetData()
+  }
+
+  render () {
+    const {isLoading, temp} = this.state
+    return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#87CEFA" barStyle="dark-content" />
+      {isLoading ? <Loading /> : <Weather temp={temp}/>}
+    </View>
+    );
+  }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#87CEFA"
+  }
+})
